4e11a6c7af67e82816a81e8e447455349b41ffdf
Aleksandar Topalovic <71898612+epicalekspwner@users.noreply.github.com>
2022-02-05 08:17:35 +0100

    Update scriptDetailed_CantonRelative.py

--- demographicsEducation/canton/scriptDetailed_CantonRelative.py
+++ demographicsEducation/canton/scriptDetailed_CantonRelative.py
@@ -1,12 +1,13 @@
-# Connect Google Drive
-from google.colab import drive
-drive.mount('/content/drive')
+# scriptDetailed_CantonRelative
+# Convert Absolute Values Into Relative Ones
+# demographicsEducation/canton
 
 # Import Pandas Library
 import pandas as pd
 
 # Set Cantons and Periodicity
-canton = ['AG', 'AI', 'AR', 'BE', 'BL', 'BS', 'FR', 'GE', 'GL', 'GR', 'JU', 'LU', 'NE', 'NW', 'OW', 'SG', 'SH', 'SO', 'SZ', 'TG', 'TI', 'UR', 'VD', 'VS', 'ZG', 'ZH']
+canton = ['AG', 'AI', 'AR', 'BE', 'BL', 'BS', 'FR', 'GE', 'GL', 'GR', 'JU', 'LU', 'NE', 
+          'NW', 'OW', 'SG', 'SH', 'SO', 'SZ', 'TG', 'TI', 'UR', 'VD', 'VS', 'ZG', 'ZH']
 periodicity = list(range(2016, 2021))
 
 # Create New Dictionary
@@ -14,9 +15,10 @@ dictURL = {}
 
 # Create Corresponding URLs and Store Them in the Dictionary Created Beforehand
 for c in canton:
-  for year in periodicity:
-    dictURL['{0}_{1}'.format(year,c)] ='https://raw.githubusercontent.com/epicalekspwner/OpenDataCustomerAnalytics/main/demographicsEducation/canton/' + str(c) + '/Absolute/'  + str(year) +'_' + str(c) + '_demographicsEducation_Absolute.csv'
-
+    for year in periodicity:
+        dictURL['{0}_{1}'.format(year, c)] = 'https://raw.githubusercontent.com/epicalekspwner/OpenDataCustomerAnalytics/main/demographicsEducation/canton/' 
+        + str(c) + '/Absolute/' + str(year) + '_' + str(c) + '_demographicsEducation_Absolute.csv'
+        
 # Get the Dictionary Keys to Access the URLs
 keys = list(dictURL.keys())
 
@@ -25,27 +27,23 @@ dictDataFrames = {}
 
 # Accessing the Original Datasets Through URLs and Converting Them Into DataFrames
 for i in range(len(keys)):
-  dictDataFrames['{0}'.format(keys[i])] = pd.read_csv(dictURL[keys[i]])
+    dictDataFrames['{0}'.format(keys[i])] = pd.read_csv(dictURL[keys[i]])
 
 # Get Column Names and Drop Useless Attribute for Relativeness
 attr = list(dictDataFrames[keys[0]].columns)
-attrToBeRemoved = ['total', 'attribute', 'total_IC','withoutPostCompulsoryEducation_IC','secondLevel2ProfessionalTraining_IC','secondLevel2GeneralTraining_IC','thirdLevelHigherProfessionalTraining_IC','thirdLevelUniversities_IC']
+attrToBeRemoved = ['total', 'attribute', 'total_IC', 'withoutPostCompulsoryEducation_IC', 'secondLevel2ProfessionalTraining_IC', 
+                   'secondLevel2GeneralTraining_IC', 'thirdLevelHigherProfessionalTraining_IC', 'thirdLevelUniversities_IC']
 for i in attrToBeRemoved:
-  attr.remove(i)
-
-# Get Column Names For IC Percent Formating
-attr_IC = ['total_IC','withoutPostCompulsoryEducation_IC','secondLevel2ProfessionalTraining_IC','secondLevel2GeneralTraining_IC','thirdLevelHigherProfessionalTraining_IC','thirdLevelUniversities_IC']
-
+    attr.remove(i)
+    
 # Iteration and Convertion Into Percentage (Within Canton)
 for i in range(len(dictDataFrames)):
-  for j in attr:
-    dictDataFrames[keys[i]][j] = round(dictDataFrames[keys[i]][j]/dictDataFrames[keys[i]]['total'], 4)
-  for k in attr_IC:
-    dictDataFrames[keys[i]][k] = round(dictDataFrames[keys[i]][k]/100, 3)
+    for j in attr:
+        dictDataFrames[keys[i]][j] = round(dictDataFrames[keys[i]][j] / dictDataFrames[keys[i]]['total'], 4)    
 for i in range(len(dictDataFrames)):
-  dictDataFrames[keys[i]]['total'] = round(dictDataFrames[keys[i]]['total']/dictDataFrames[keys[i]]['total'], 4)
+    dictDataFrames[keys[i]]['total'] = round(dictDataFrames[keys[i]]['total'] / dictDataFrames[keys[i]]['total'], 4)
     
 # Export DataFrames
 for i in dictDataFrames.keys():
-  exportLink = '/content/drive/MyDrive/Datasets1/{0}/{1}_demographicsEducationCantonRelative.csv'.format(i[-2:],i)
-  dictDataFrames[i].to_csv(exportLink, na_rep = 'NaN', index = False)
+    exportLink = 'C:/Users/aleks/OneDrive/Documents/GitHub/OpenDataCustomerAnalytics/demographicsEducation/canton/{0}/CantonRelative/{1}_demographicsEducation_CantonRelative.csv'.format(i[-2:], i)
+    dictDataFrames[i].to_csv(exportLink, na_rep='NaN', index=False)
