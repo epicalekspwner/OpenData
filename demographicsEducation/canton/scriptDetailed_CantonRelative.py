2f4a00de9e8201413574f33b87540c22e0c24196
Aleksandar Topalovic <71898612+epicalekspwner@users.noreply.github.com>
2022-02-07 18:53:49 +0100

    Update scriptDetailed_CantonRelative.py

--- demographicsEducation/canton/scriptDetailed_CantonRelative.py
+++ demographicsEducation/canton/scriptDetailed_CantonRelative.py
@@ -2,8 +2,9 @@
 # Convert Absolute Values Into Relative Ones (Within Canton)
 # /demographicsEducation/canton/
 
-# Import Pandas Library
+# Import Pandas and OS Libraries
 import pandas as pd
+import os
 
 # Set Cantons and Periodicity
 canton = ['AG', 'AI', 'AR', 'BE', 'BL', 'BS', 'FR', 'GE', 'GL', 'GR', 'JU', 'LU', 'NE', 'NW', 'OW', 'SG', 'SH', 'SO', 'SZ', 'TG', 'TI', 'UR', 'VD', 'VS', 'ZG', 'ZH']
@@ -16,7 +17,7 @@ dictURL = {}
 for c in canton:
     for year in periodicity:
         dictURL['{0}_{1}'.format(year, c)] = 'https://raw.githubusercontent.com/epicalekspwner/OpenDataCustomerAnalytics/main/demographicsEducation/canton/' + str(c) + '/Absolute/' + str(year) + '_' + str(c) + '_demographicsEducation_Absolute.csv'
-        
+
 # Get the Dictionary Keys to Access the URLs
 keys = list(dictURL.keys())
 
@@ -32,15 +33,17 @@ attr = list(dictDataFrames[keys[0]].columns)
 attrToBeRemoved = ['attribute', 'total', 'total_IC', 'withoutPostCompulsoryEducation_IC', 'secondLevel2ProfessionalTraining_IC', 'secondLevel2GeneralTraining_IC', 'thirdLevelHigherProfessionalTraining_IC', 'thirdLevelUniversities_IC']
 for i in attrToBeRemoved:
     attr.remove(i)
-    
+
 # Iteration and Convertion Into Percentage (Within Canton)
 for i in range(len(dictDataFrames)):
     for j in attr:
-        dictDataFrames[keys[i]][j] = round(dictDataFrames[keys[i]][j] / dictDataFrames[keys[i]]['total'], 4)    
-for i in range(len(dictDataFrames)):
-    dictDataFrames[keys[i]]['total'] = round(dictDataFrames[keys[i]]['total'] / dictDataFrames[keys[i]]['total'], 4)
-    
-# Export DataFrames
+        dictDataFrames[keys[i]][j] = round(dictDataFrames[keys[i]][j] / dictDataFrames[keys[i]]['total'], 4)
+
+# Export Data With Automated Files Management
 for i in dictDataFrames.keys():
-    exportLink = 'C:/Users/aleks/OneDrive/Documents/GitHub/OpenDataCustomerAnalytics/demographicsEducation/canton/{0}/CantonRelative/{1}_demographicsEducation_CantonRelative.csv'.format(i[-2:], i)
-    dictDataFrames[i].to_csv(exportLink, na_rep='NaN', index=False)
+    exportPath = 'C:/GitHub/OpenDataCustomerAnalytics/demographicsEducation/canton/{0}/CantonRelative/'.format(i[-2:])
+    exportPathExist = os.path.exists(exportPath)
+    if not exportPathExist:
+        os.makedirs(exportPath)
+    exportFile = 'C:/GitHub/OpenDataCustomerAnalytics/demographicsEducation/canton/{0}/CantonRelative/{1}_demographicsEducation_CantonRelative.csv'.format(i[-2:], i)
+    dictDataFrames[i].to_csv(exportFile, na_rep='NaN', index=False)
